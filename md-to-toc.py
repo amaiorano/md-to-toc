cab9d19671ce80d6cfd022a67670cf290c9016e4
Antonio Maiorano <amaiorano@gmail.com>
2015-06-01 08:34:14 -0400

    Remove leading spaces so that output is flush with left edge

--- md-to-toc.py
+++ md-to-toc.py
@@ -12,11 +12,15 @@ HEADER_LINE_RE = re.compile("^(#+)\s*(.*?)\s*(#+$|$)", re.IGNORECASE)
 # Dictionary of anchor name to number of instances found so far
 anchors = {}
 
+
 def print_usage():
 	print("\nUsage: md-to-toc <markdown_file>")
 
 def to_github_anchor(title):
-	'''Converts markdown header title (without #s) to GitHub-formatted anchor'''
+	'''
+	Converts markdown header title (without #s) to GitHub-formatted anchor.
+	Note that this function attempts to recreate GitHub's anchor-naming logic.
+	'''
 	
 	# Convert to lower case and replace spaces with dashes
 	anchor_name = title.strip().lower().replace(' ', '-')
@@ -26,7 +30,7 @@ def to_github_anchor(title):
 	
 	# If we've encountered this anchor name before, append next instance count
 	count = anchors.get(anchor_name)
-	if (count == None):
+	if count == None:
 		anchors[anchor_name] = 0
 	else:
 		count = count + 1
@@ -35,10 +39,6 @@ def to_github_anchor(title):
 	
 	return '#' + anchor_name
 	
-#TODO: Find a library that does this
-def to_web_string(s):
-	return s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
-
 def toggles_block_quote(line):
 	'''Returns true if line toggles block quotes on or off (i.e. finds odd number of ```)'''
 	n = line.count("```")
@@ -55,14 +55,15 @@ def main(argv = None):
 	filespec = argv[1]
 
 	in_block_quote = False
+	results = [] # list of (header level, title, anchor) tuples
 
 	file = open(filespec)
 	for line in file.readlines():
 
-		if (toggles_block_quote(line)):
+		if toggles_block_quote(line):
 			in_block_quote = not in_block_quote;
 
-		if (in_block_quote):
+		if in_block_quote:
 			continue
 
 		m = HEADER_LINE_RE.match(line)
@@ -70,7 +71,15 @@ def main(argv = None):
 			header_level = len(m.group(1))
 			title = m.group(2)
 			spaces = "  " * (header_level - 1)
-			print("{}{} [{}]({})".format(spaces, TOC_LIST_PREFIX, to_web_string(title), to_github_anchor(title)))
+			results.append( (header_level, title, to_github_anchor(title)) )
+			
+	# Compute min header level so we can offset output to be flush with left edge
+	min_header_level = min(results, key=lambda e: e[0])[0]
+			
+	for r in results:
+		header_level = r[0]
+		spaces = "  " * (header_level - min_header_level)
+		print("{}{} [{}]({})".format(spaces, TOC_LIST_PREFIX, r[1], r[2]))
 
 if __name__ == "__main__":
 	sys.exit(main())
