c5ea9424bd6236677c2b9301629c693181b75fc2
Chris Jones <cmsj@tenshu.net>
2016-07-20 13:17:48 +0100

    Fix pep8 issues.

--- md-to-toc.py
+++ md-to-toc.py
@@ -1,6 +1,5 @@
 # Author: Antonio Maiorano (amaiorano@gmail.com)
 
-import os
 import sys
 import re
 
@@ -14,95 +13,101 @@ HEADER2_UNDERLINE_RE = re.compile("^=+$")
 # Dictionary of anchor name to number of instances found so far
 anchors = {}
 
+
 def print_usage():
-	print("\nUsage: md-to-toc <markdown_file>")
+    print("\nUsage: md-to-toc <markdown_file>")
+
 
 def to_github_anchor(title):
-	'''
-	Converts markdown header title (without #s) to GitHub-formatted anchor.
-	Note that this function attempts to recreate GitHub's anchor-naming logic.
-	'''
+    '''
+    Converts markdown header title (without #s) to GitHub-formatted anchor.
+    Note that this function attempts to recreate GitHub's anchor-naming logic.
+    '''
 
-	# Convert to lower case and replace spaces with dashes
-	anchor_name = title.strip().lower().replace(' ', '-')
+    # Convert to lower case and replace spaces with dashes
+    anchor_name = title.strip().lower().replace(' ', '-')
 
-	# Strip all invalid characters
-	anchor_name = re.sub("[^A-Za-z0-9\-_]", "", anchor_name)
+    # Strip all invalid characters
+    anchor_name = re.sub("[^A-Za-z0-9\-_]", "", anchor_name)
 
-	# If we've encountered this anchor name before, append next instance count
-	count = anchors.get(anchor_name)
-	if count == None:
-		anchors[anchor_name] = 0
-	else:
-		count = count + 1
-		anchors[anchor_name] = count
-		anchor_name = anchor_name + '-' + str(count)
+    # If we've encountered this anchor name before, append next instance count
+    count = anchors.get(anchor_name)
+    if count is None:
+        anchors[anchor_name] = 0
+    else:
+        count = count + 1
+        anchors[anchor_name] = count
+        anchor_name = anchor_name + '-' + str(count)
 
-	return '#' + anchor_name
+    return '#' + anchor_name
+
 
 def toggles_block_quote(line):
-	'''Returns true if line toggles block quotes on or off (i.e. finds odd number of ```)'''
-	n = line.count("```")
-	return n > 0 and line.count("```") % 2 != 0
+    '''Returns true if line toggles block quotes on or off'''
+    '''(i.e. finds odd number of ```)'''
+    n = line.count("```")
+    return n > 0 and line.count("```") % 2 != 0
 
-def main(argv = None):
-	if argv is None:
-		argv = sys.argv
 
-	if len(argv) < 2:
-		print_usage()
-		return 0
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
 
-	filespec = argv[1]
+    if len(argv) < 2:
+        print_usage()
+        return 0
 
-	in_block_quote = False
-	results = [] # list of (header level, title, anchor) tuples
-	last_line = ""
+    filespec = argv[1]
 
-	file = open(filespec)
-	for line in file.readlines():
+    in_block_quote = False
+    results = []  # list of (header level, title, anchor) tuples
+    last_line = ""
 
-		if toggles_block_quote(line):
-			in_block_quote = not in_block_quote;
+    file = open(filespec)
+    for line in file.readlines():
 
-		if in_block_quote:
-			continue
+        if toggles_block_quote(line):
+            in_block_quote = not in_block_quote
 
-		found_header = False
-		header_level = 0
+        if in_block_quote:
+            continue
 
-		m = HEADER_LINE_RE.match(line)
-		if m != None:
-			header_level = len(m.group(1))
-			title = m.group(2)
-			found_header = True
+        found_header = False
+        header_level = 0
 
-		if not found_header:
-			m = HEADER1_UNDERLINE_RE.match(line)
-			if m != None:
-				header_level = 1
-				title = last_line.rstrip()
-				found_header = True
+        m = HEADER_LINE_RE.match(line)
+        if m is not None:
+            header_level = len(m.group(1))
+            title = m.group(2)
+            found_header = True
 
-		if not found_header:
-			m = HEADER2_UNDERLINE_RE.match(line)
-			if m != None:
-				header_level = 2
-				title = last_line.rstrip()
-				found_header = True
+        if not found_header:
+            m = HEADER1_UNDERLINE_RE.match(line)
+            if m is not None:
+                header_level = 1
+                title = last_line.rstrip()
+                found_header = True
 
-		if found_header:
-			results.append( (header_level, title, to_github_anchor(title)) )
+        if not found_header:
+            m = HEADER2_UNDERLINE_RE.match(line)
+            if m is not None:
+                header_level = 2
+                title = last_line.rstrip()
+                found_header = True
 
-		last_line = line
+        if found_header:
+            results.append((header_level, title, to_github_anchor(title)))
 
-	# Compute min header level so we can offset output to be flush with left edge
-	min_header_level = min(results, key=lambda e: e[0])[0]
+        last_line = line
 
-	for r in results:
-		header_level = r[0]
-		spaces = "  " * (header_level - min_header_level)
-		print("{}{} [{}]({})".format(spaces, TOC_LIST_PREFIX, r[1], r[2]))
+    # Compute min header level so we can offset output to be flush with
+    # left edge
+    min_header_level = min(results, key=lambda e: e[0])[0]
+
+    for r in results:
+        header_level = r[0]
+        spaces = "  " * (header_level - min_header_level)
+        print("{}{} [{}]({})".format(spaces, TOC_LIST_PREFIX, r[1], r[2]))
 
 if __name__ == "__main__":
-	sys.exit(main())
+    sys.exit(main())
